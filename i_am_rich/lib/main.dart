16804da7405db3175f762f98a3d0bca216198ec2
MaxmillianShetto <max.shetto@gmail.com>
2023-05-03 16:04:05 +0200

    Displayed an image and centered using the Center widget

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,12 +1,25 @@
-
 import 'package:flutter/material.dart';
 
-//The main function is the starting point for all our flutter apps
+// The main function is the starting point for all our flutter apps
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        // This is another way of specifying the shade of the color you want
+        // backgroundColor: Colors.blueGrey.shade200,
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          // This is one way of specifying the shade of the color you want
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+              'https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive.jpg',
+            ),
+          ),
+        ),
       ),
     ),
   );
