e9e7610df80d6a6bf2538c754e72e18d1fa3f4ff
MaxmillianShetto <max.shetto@gmail.com>
2023-05-03 16:11:25 +0200

    Done with Flutter:Part 02 Buidling Apps, Scaffolding a MaterialApp

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -5,22 +5,21 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        // This is another way of specifying the shade of the color you want
-        // backgroundColor: Colors.blueGrey.shade200,
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: const Text('I Am Rich'),
-          // This is one way of specifying the shade of the color you want
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: const Center(
-          child: Image(
-            image: NetworkImage(
-              'https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive.jpg',
-            ),
+          // This is another way of specifying the shade of the color you want
+          // backgroundColor: Colors.blueGrey.shade200,
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: const Text('I Am Rich'),
+            // This is one way of specifying the shade of the color you want
+            backgroundColor: Colors.blueGrey[900],
           ),
-        ),
-      ),
+          body: const Center(
+            child: Image(
+              image: NetworkImage(
+                'https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive.jpg',
+              ),
+            ),
+          )),
     ),
   );
 }
