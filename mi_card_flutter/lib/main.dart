25e788ba72996c535702d4781d44a99ed9e659e1
MaxmillianShetto <max.shetto@gmail.com>
2023-05-04 19:38:21 +0200

    Flutter: Part 03 Flutter Widgets: Exploring Container, Row and Column widgets

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -10,7 +10,39 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          // mainAxisSize: MainAxisSize.min, this minimizes vertical height to height of items
+          // verticalDirection: VerticalDirection.down, This arranges content from bottom to top
+          //    To change the spacing arrange together
+          //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // Stretches children across the screen
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 100.0,
+              color: Colors.white,
+              child: Text('Container 1'),
+            ),
+
+            // Putting some spacing in between
+            SizedBox(height: 20.0,),
+            Container(
+              height: 100.0,
+              color: Colors.blue,
+              child: Text('Container 2'),
+            ),
+            Container(
+              height: 100.0,
+              color: Colors.red,
+              child: Text('Container 3'),
+            ),
+            Container(
+              width: double.infinity,
+              height: 100,
+            )
+          ],
+        )),
       ),
     );
   }
