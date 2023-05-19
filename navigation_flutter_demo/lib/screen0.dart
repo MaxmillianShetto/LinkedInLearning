cd7c6a6b8f118aa8f90997bc017ef042c1588f52
MaxmillianShetto <max.shetto@gmail.com>
2023-05-19 18:13:28 +0200

    Flutter 07: Building UIs ... Navigation Demo

---
+++ navigation_flutter_demo/lib/screen0.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Screen0 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text('Screen 0'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.red),
+              child: Text('Go To Screen 1',
+                style: TextStyle(
+                  color: Colors.white,
+                ),),
+              onPressed: () {
+                //Navigate to Screen 1
+                Navigator.pushNamed(context, '/first');
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.blue),
+              child: Text('Go To Screen 2',
+                style: TextStyle(
+                  color: Colors.white,
+                ),),
+              onPressed: () {
+                //Navigate to Screen 2
+                Navigator.pushNamed(context, '/second');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
